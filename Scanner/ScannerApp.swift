85d0a876bd51ab0678ea35f054ab90251a628607
Matthew Houston <matthew.houston@hyland.com>
2025-12-28 15:31:16 -0800

    Initial Commit

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Matthew Houston on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
